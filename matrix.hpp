225555fc5480f22aee107e00c09450e07a260216
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-07-21 17:48:48 +0200

    added constant identity matricies

--- matrix.hpp
+++ matrix.hpp
@@ -500,4 +500,8 @@ typedef Matrix<2, 1> Vector2;
 typedef Matrix<3, 1> Vector3;
 typedef Matrix<4, 1> Vector4;
 
+const Matrix<2, 2> MatrixI2(IDENTITY);
+const Matrix<3, 3> MatrixI3(IDENTITY);
+const Matrix<4, 4> MatrixI4(IDENTITY);
+
 #endif // _V_MATRIX_HPP
